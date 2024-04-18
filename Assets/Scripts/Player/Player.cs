5f55aa80d0b5369c1bb57525a36f7bd6ea67eb49
Keke Brown <fbrowndev@gmail.com>
2024-04-18 03:01:01 -0400

    Player Jump, gravity, and velocity added

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,7 +6,15 @@ public class Player : MonoBehaviour
 {
     #region Player Variables
     [Header("Movement Settings")]
-    [SerializeField]private float _moveSpeed = 2f;
+    [SerializeField] private float _moveSpeed = 2f;
+    [SerializeField] private float _jumpHeight = 15f;
+    [SerializeField] private float _doubleJumpMultiplier = 1.5f;
+    [SerializeField] private float _gravity = 1.0f;
+
+    private bool _canDoubleJump;
+
+    //Cached values
+    private float _yVelocity;
 
     //Components
     private CharacterController _characterController;
@@ -36,8 +44,30 @@ public class Player : MonoBehaviour
         Vector3 _moveDirection = new Vector3(_horizontalInput, 0,0);
         Vector3 _playerVelocity = _moveDirection * _moveSpeed;
 
-        _characterController.Move(_playerVelocity * Time.deltaTime);
+        if(_characterController.isGrounded)
+        {
+            //player jump
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _canDoubleJump = true;
+            }
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                if (_canDoubleJump == true)
+                {
+                    _yVelocity += _jumpHeight * _doubleJumpMultiplier;
+                    _canDoubleJump = false;
+                }    
+            }
+            _yVelocity -= _gravity;
+        }
 
+        _playerVelocity.y = _yVelocity;
+        _characterController.Move(_playerVelocity * Time.deltaTime);
     }
     #endregion
 
